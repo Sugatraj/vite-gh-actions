feda3d41c4ac21e4409c5d8b7f1df9c669010afe
Sugatraj <sugatraj.2106@gmail.com>
2025-03-06 01:31:48 +0530

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import 'bootstrap'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="container min-vh-100 d-flex justify-content-center align-items-center">
+        <div className="d-flex flex-column align-items-center">
+          <div className="fs-1 fw-bold">Jai Vite 🔥</div>
+          <hr className="w-100 bg-danger" />
+          <div className="text-dark fs-5 mt-3">
+            This is a simple Vite + React project just to learn <b>Vite</b>, <b>Github pages</b> deployment with <b>Github Actions</b>.
+            <br />
+            <span className="d-flex justify-content-end fw-bold">
+              Date: 06 March 2025
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
